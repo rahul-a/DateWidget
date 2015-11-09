486d58d5b31bc24653ec9eb3190ac7c7d5a10ff9
Priyabrata Patnaik <priyabrata.patnaik@treebohotels.com>
2015-11-10 00:12:24 +0530

    added a custom layout manager to adjust scroll speed

---
+++ app/src/main/java/com/sample/datewidget/activities/CustomLayoutManager.java
@@ -0,0 +1,43 @@
+package com.sample.datewidget.activities;
+
+import android.content.Context;
+import android.graphics.PointF;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSmoothScroller;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by priyabratapatnaik on 09/11/15.
+ */
+public class CustomLayoutManager extends LinearLayoutManager {
+    private static final float MILLISECONDS_PER_INCH = 200f;
+    private Context mContext;
+
+    public CustomLayoutManager(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public void smoothScrollToPosition(RecyclerView recyclerView, RecyclerView.State state, final int position) {
+
+        LinearSmoothScroller smoothScroller = new LinearSmoothScroller(mContext) {
+
+            //This controls the direction in which smoothScroll looks for your view
+            @Override
+            public PointF computeScrollVectorForPosition(int targetPosition) {
+                return computeScrollVectorForPosition(targetPosition);
+            }
+
+            //This returns the milliseconds it takes to scroll one pixel.
+            @Override
+            protected float calculateSpeedPerPixel(DisplayMetrics displayMetrics) {
+                return MILLISECONDS_PER_INCH / displayMetrics.densityDpi;
+            }
+        };
+
+        smoothScroller.setTargetPosition(position);
+        startSmoothScroll(smoothScroller);
+    }
+}
